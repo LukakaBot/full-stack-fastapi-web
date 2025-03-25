ac7b2bd4568515b275cc44cfe9d92d335885cfd2
鲨鱼辣椒 <monishine_code@163.com>
2025-03-25 16:39:49 +0800

    feat(deps-dev): 添加 unplugin-auto-import

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
@@ -8,6 +9,11 @@ export default defineConfig(() => {
     plugins: [
       react(),
       tailwindcss(),
+      AutoImport({
+        imports: ["react", "react-router"],
+        dts: "src/types/auto-import.d.ts",
+        include: [/\.[tj]sx?$/, /\.md$/],
+      }),
     ],
   }
 })
