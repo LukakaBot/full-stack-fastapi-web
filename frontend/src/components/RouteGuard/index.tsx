912eb4406e73cd92912f5da8b632b7f10822562a
鲨鱼辣椒 <monishine_code@163.com>
2025-03-26 17:07:50 +0800

    feat(comp): 添加RouteGuard组件

---
+++ frontend/src/components/RouteGuard/index.tsx
@@ -0,0 +1,22 @@
+import { Navigate } from 'react-router';
+import { useRouter } from '@/hook/index';
+interface RouteGuardProps {
+	children: React.ReactNode;
+}
+
+function RouteGuard({ children }: RouteGuardProps) {
+	const token = window.$bucket?.get('token');
+	const { currentRoute } = useRouter();
+
+	if (!token && currentRoute.pathname !== '/login') {
+		return <Navigate to='/login' state={{ from: currentRoute }} replace />;
+	}
+
+	if (token && currentRoute.pathname === '/login') {
+		return <Navigate to='/system/user' replace />;
+	}
+
+	return <>{children}</>;
+}
+
+export default RouteGuard;
