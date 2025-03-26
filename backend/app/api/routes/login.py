eb1fe94700314e335f0cb91d75645c034157e512
鲨鱼辣椒 <monishine_code@163.com>
2025-03-26 14:59:50 +0800

    feat(api): 改造登录接口，添加获取用户信息接口

--- backend/app/api/routes/login.py
+++ backend/app/api/routes/login.py
@@ -1,8 +1,7 @@
-import fastapi
 from fastapi import APIRouter
-from pydantic import BaseModel
 from faker import Faker
-
+from app.model.common import ResponseData
+from app.model.login import AccountUserTokenParams, AccountUserTokenResponse, UserInfo
 
 router = APIRouter(tags=["login"])
 
@@ -10,21 +9,31 @@ router = APIRouter(tags=["login"])
 fake = Faker()
 
 
-class UserFormData(BaseModel):
-    username: str
-    password: str
-
-
 @router.post("/user/account/token")
-def get_access_token(form_data: UserFormData):
-    # if form_data.username != "admin" or form_data.password != "123":
-    #     raise fastapi.HTTPException(status_code=500, detail="用户名或密码错误")
+def get_access_token(form_data: AccountUserTokenParams):
+    return ResponseData(
+        code=200,
+        data={
+            "userId": fake.random_int(min=100000, max=999999),
+            "username": fake.name(),
+            "token": fake.uuid4(),
+        },
+    )
 
-    return {
-        "username": form_data.username,
-        "realName": fake.name(),
-        "phone": fake.phone_number(),
-        "age": fake.random_int(min=18, max=60),
-        "sex": fake.random_element(elements=("男", "女")),
-        "token": fake.uuid4(),
-    }
+
+@router.post("/user/info")
+def get_user_info(token: str):
+    return ResponseData(
+        code=200,
+        data={
+            "userId": fake.random_int(min=100000, max=999999),
+            "userName": fake.name(),
+            "avatar": fake.image_url(),
+            "roles": ["admin"],
+            "roleName": "admin",
+            "menus": [],
+            "token": token,
+            "organizeId": fake.random_int(min=100000, max=999999),
+            "organizeName": fake.company(),
+        },
+    )
