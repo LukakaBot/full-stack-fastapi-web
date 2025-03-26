40da767234bf3e6f5a5fbd9b21078d13738108fa
鲨鱼辣椒 <monishine_code@163.com>
2025-03-26 11:37:39 +0800

    feat: 修改前端的网络请求配置

--- frontend/src/utils/service/index.ts
+++ frontend/src/utils/service/index.ts
@@ -2,9 +2,15 @@ import { createAlova } from "alova";
 import adapterFetch from "alova/fetch";
 import interceptorManager from "./core/interceptorManager";
 
+const { VITE_SERVICE_URL, VITE_SERVICE_PORT, VITE_SERVICE_PREFIX } = import.meta.env
+
+const SERVICE_PORT = `${VITE_SERVICE_PORT ? `:${VITE_SERVICE_PORT}` : ''}`
+
+const BASE_URL = `${VITE_SERVICE_URL}${SERVICE_PORT}${VITE_SERVICE_PREFIX}`
 
 const service = createAlova({
-  baseURL: '/api',
+  baseURL: BASE_URL,
+  timeout: 1000 * 8,
   requestAdapter: adapterFetch(),
   beforeRequest: interceptorManager.beforeRequest,
   responded: interceptorManager.responded
