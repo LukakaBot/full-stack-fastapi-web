f0b361cb9288fe48509d8dc2a48d9c71f6bb03f4
鲨鱼辣椒 <monishine_code@163.com>
2025-03-26 16:08:29 +0800

    feat(page): 登录按钮添加loading状态

--- frontend/src/pages/login/components/LoginBox/index.tsx
+++ frontend/src/pages/login/components/LoginBox/index.tsx
@@ -1,18 +1,23 @@
 import { Card, Form, Input, Button } from 'antd';
 import type { AccountUserTokenParams } from '@/api/user/types';
 import { fetchAccountUserToken } from '@/api/user';
+import { useRouter, useLoading } from '@/hook';
 
 function LoginBox() {
 	const [formData] = useState<AccountUserTokenParams>({
 		username: 'admin',
 		password: '123456',
 	});
+	const router = useRouter();
+	const [loading, setLoading] = useLoading();
 
 	async function handleFinish(value: AccountUserTokenParams) {
 		try {
+			setLoading(true);
 			await fetchAccountUserToken(value);
-		} catch (error) {
-			console.error(error);
+			router.replace('/');
+		} finally {
+			setLoading(false);
 		}
 	}
 
@@ -32,7 +37,7 @@ function LoginBox() {
 						<Input.Password />
 					</Form.Item>
 					<Form.Item>
-						<Button block type='primary' htmlType='submit'>
+						<Button block type='primary' htmlType='submit' loading={loading}>
 							登录
 						</Button>
 					</Form.Item>
